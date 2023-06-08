c9644dec4d270df6bb84ebe8a3068d160da647e1
Muhammad Masood <yoursourcecode@gmail.com>
2023-06-07 20:04:58 -0700

    deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers"
+import "dotenv/config";
+
+const priv_key = process.env.private_key;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-};
+  defaultNetwork: "bscTestnet",
+  networks:{
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: [priv_key as string],
+  }
+},
+}
 
 export default config;
