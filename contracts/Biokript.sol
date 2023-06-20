c5f718b97ed777c9edc4c8922e52d640ca5a3c38
Muhammad Masood <yoursourcecode@gmail.com>
2023-06-20 11:01:58 -0700

    contract

--- contracts/Biokript.sol
+++ contracts/Biokript.sol
@@ -28,10 +28,10 @@ interface IPancakeV2Pair {
 
     function balanceOf(address owner) external view returns (uint256);
 
-    function allowance(
-        address owner,
-        address spender
-    ) external view returns (uint256);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
 
     function approve(address spender, uint256 value) external returns (bool);
 
@@ -87,7 +87,11 @@ interface IPancakeV2Pair {
     function getReserves()
         external
         view
-        returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
 
     function price0CumulativeLast() external view returns (uint256);
 
@@ -97,9 +101,9 @@ interface IPancakeV2Pair {
 
     function mint(address to) external returns (uint256 liquidity);
 
-    function burn(
-        address to
-    ) external returns (uint256 amount0, uint256 amount1);
+    function burn(address to)
+        external
+        returns (uint256 amount0, uint256 amount1);
 
     function swap(
         uint256 amount0Out,
@@ -127,19 +131,18 @@ interface IPancakeV2Factory {
 
     function feeToSetter() external view returns (address);
 
-    function getPair(
-        address tokenA,
-        address tokenB
-    ) external view returns (address pair);
+    function getPair(address tokenA, address tokenB)
+        external
+        view
+        returns (address pair);
 
     function allPairs(uint256) external view returns (address pair);
 
     function allPairsLength() external view returns (uint256);
 
-    function createPair(
-        address tokenA,
-        address tokenB
-    ) external returns (address pair);
+    function createPair(address tokenA, address tokenB)
+        external
+        returns (address pair);
 
     function setFeeTo(address) external;
 
@@ -160,7 +163,13 @@ interface IPancakeV2Router01 {
         uint256 amountBMin,
         address to,
         uint256 deadline
-    ) external returns (uint256 amountA, uint256 amountB, uint256 liquidity);
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
 
     function addLiquidityBNB(
         address token,
@@ -172,7 +181,11 @@ interface IPancakeV2Router01 {
     )
         external
         payable
-        returns (uint256 amountToken, uint256 amountBNB, uint256 liquidity);
+        returns (
+            uint256 amountToken,
+            uint256 amountBNB,
+            uint256 liquidity
+        );
 
     function removeLiquidity(
         address tokenA,
@@ -284,15 +297,15 @@ interface IPancakeV2Router01 {
         uint256 reserveOut
     ) external pure returns (uint256 amountIn);
 
-    function getAmountsOut(
-        uint256 amountIn,
-        address[] calldata path
-    ) external view returns (uint256[] memory amounts);
+    function getAmountsOut(uint256 amountIn, address[] calldata path)
+        external
+        view
+        returns (uint256[] memory amounts);
 
-    function getAmountsIn(
-        uint256 amountOut,
-        address[] calldata path
-    ) external view returns (uint256[] memory amounts);
+    function getAmountsIn(uint256 amountOut, address[] calldata path)
+        external
+        view
+        returns (uint256[] memory amounts);
 }
 
 interface IPancakeV2Router02 is IPancakeV2Router01 {
@@ -342,13 +355,9 @@ interface IPancakeV2Router02 is IPancakeV2Router01 {
     ) external;
 }
 
-contract BioKript is ERC20, Ownable, ReentrancyGuard  {
-
+contract BioKript is ERC20, Ownable, ReentrancyGuard {
     using SafeMath for uint256;
-    IPancakeV2Router02 public pancakeV2Router;
     address public immutable pancakeV2Pair;
-
-    bool private swapping;
 
     //all profits/revenues will be held here
     address private distributor;
@@ -357,89 +366,82 @@ contract BioKript is ERC20, Ownable, ReentrancyGuard  {
     address private safetyFunds;
     address private reserves;
     address private controller;
+    address private liquidityReceiver;
+    address private feeReceiver;
 
-    // trading status
-    bool public isTradingEnabled;
     // exlcude from fees and max transaction amount
     mapping(address => bool) public _isExcludedFromFees;
 
-    uint256 public holdThreshold = 1000000 * 10 ** 18;
+    uint256 public holdThreshold = 1000000 * 10**18;
 
     //Buy and Sell Tax
-    uint256 public buyTax = 3; //3%
-    uint256 public sellTax = 3; //3%
-    uint256 public liquidityFee = 3; //3%
-
-    //Max Swap Limit
-    bool public isSwapLimitEnabled;
-    uint256 public MAX_SWAP_LIMIT = 3; //3 Tokens
+    uint256 public buyTax; // 3% Buy Tax
+    uint256 public sellTax; // 3% Sell Tax
+    uint256 public liquidityFee; // 2% liquidity fee
 
     //distribution and rewards
-    mapping (address => uint256) private claimDur;
-    uint256 public nextDistr;  //next distribution
+    mapping(address => uint256) private claimDur;
+    uint256 public nextDistr; //next distribution
     uint256 public distributeTokens;
-    mapping (address => bool) public rewardClaimed;
     // Initiate this this current balance when user comes to platform first.
-    mapping(address=>uint256) private previousBalance;
+    mapping(address => uint256) private previousBalance;
+    mapping(address => bool) private _isLiquidityPair;
 
     event ExcludeFromFees(address indexed account, bool isExcluded);
 
-    // event GasForProcessingUpdated(uint256 indexed newValue, uint256 indexed oldValue);
+    event UpdatePair(address indexed _address, bool status);
 
-    event SwapAndLiquify(
-        uint256 tokensSwapped,
-        uint256 bnbReceived,
-        uint256 tokensIntoLiqudity
-    );
-
-    constructor() ERC20("BKPT", "BKPT") {
-        distributor = 0x5d135b21EcC60000B0ecDE932617105f04F71EFB;
-        controller = 0x83bB8802e28fea889AAF7D8F56ee286af0002409;
+    constructor() ERC20("Biokript", "BKPT") {
+        distributor = 0x6a07bB46D93c4BF298E1aCf67bDBd163e7B793c6;//0xa07be378B303cEbb44B6692819ED1910BDc906C6;
+        controller = 0x6a07bB46D93c4BF298E1aCf67bDBd163e7B793c6;//0xa07be378B303cEbb44B6692819ED1910BDc906C6;
         companyWallet = 0x62537084Ad89F5137bad17135cb98D88b971EcbF;
         burnWallet = 0x24C6D745f762be4ad8f165d4a3E0E7c35E443E6E;
         safetyFunds = 0x0DC0ed454b001680B239c55b1bec76F8f93C609D;
         reserves = 0xF41D58658fA316CEEfA20F781424661c1Db5CfBf;
+        liquidityReceiver = 0xe1a2967DeAb90287cbE1c57768a0d347ccd8f530;
+        feeReceiver = 0x4ed2D84a05DB7212039B4290507762Da79D4Ceb7;
+
         IPancakeV2Router02 _pancakeV2Router = IPancakeV2Router02(
-            0xD99D1c33F9fC3444f8101754aBC46c52416550D1
+            0xD99D1c33F9fC3444f8101754aBC46c52416550D1  
         ); //(0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3);//(0x10ED43C718714eb63d5aA57B78B54704E256024E);
         // Create a pancake pair for this new token
         pancakeV2Pair = IPancakeV2Factory(_pancakeV2Router.factory())
             .createPair(address(this), _pancakeV2Router.WETH());
 
-        pancakeV2Router = _pancakeV2Router;
+        _isLiquidityPair[pancakeV2Pair] = true;
+
         // exclude from paying fees or having max transaction amount
         excludeFromFees(owner(), true);
         excludeFromFees(address(this), true);
-        nextDistr = block.timestamp + 3 minutes;
+        nextDistr = block.timestamp;
         /*
             _mint is an internal function in ERC20.sol that is only called here,
             and CANNOT be called ever again
         */
-        _mint(owner(), 500000000 * 10 ** 18);
+
+        // Initially set high tax (Anti-bot)
+        buyTax = 50;
+        sellTax = 50;
+
+        _mint(owner(), 500000000 * 10**18);
         emit Transfer(address(0), msg.sender, totalSupply());
     }
 
-    function setBuyTax(uint256 _tax) external onlyOwner {
+    function setBuyTax(uint256 _tax) external onlyController {
         buyTax = _tax;
     }
 
-    function setSellTax(uint256 _tax) external onlyOwner {
+    function setSellTax(uint256 _tax) external onlyController {
         sellTax = _tax;
     }
 
-    function setLiquidityFee(uint256 _fee) external onlyOwner {
+    function setLiquidityFee(uint256 _fee) external onlyController {
         liquidityFee = _fee;
     }
 
-    function setMaxHoldLimit(uint256 _amount) external onlyOwner {
+    function setMaxHoldLimit(uint256 _amount) external onlyController {
         require(_amount > 0);
         holdThreshold = _amount;
-    }
-
-    function setSwapLimit(bool _status, uint256 _amount) external onlyOwner {
-        require(_amount > 0);
-        isSwapLimitEnabled = _status;
-        MAX_SWAP_LIMIT = _amount;
     }
 
     modifier onlyController() {
@@ -448,14 +450,13 @@ contract BioKript is ERC20, Ownable, ReentrancyGuard  {
     }
 
     function burn(uint256 amount) external onlyController {
-        uint256 a = (amount * 10 ** 18);
-        _burn(msg.sender, a);
+        _burn(msg.sender, amount);
     }
 
-    function excludeFromFees(
-        address account,
-        bool excluded
-    ) public onlyController {
+    function excludeFromFees(address account, bool excluded)
+        public
+        onlyController
+    {
         require(
             _isExcludedFromFees[account] != excluded,
             " Account is already the value of 'excluded'"
@@ -465,25 +466,21 @@ contract BioKript is ERC20, Ownable, ReentrancyGuard  {
         emit ExcludeFromFees(account, excluded);
     }
 
-    function enableTrade(bool _status) external onlyOwner returns (bool) {
-        return isTradingEnabled = _status;
-    }
-
     function calcTax(
         address _from,
         address _to,
         uint256 _amount
-    ) internal view returns (uint256) {
-        //  require(isTradingEnabled,"Trading is not enabled");
-        if (_from == pancakeV2Pair) {
-            uint256 lFee = (_amount * liquidityFee) / 100;
-            return (((_amount * buyTax) / 100) + lFee);
-        } else if (_to == pancakeV2Pair) {
-            uint256 lFee = (_amount * liquidityFee) / 100;
-            return (((_amount * sellTax) / 100) + lFee);
-        }
-        else {
-            return 0;
+    ) internal view returns (uint256, uint256) {
+        uint256 liqFee = 0;
+        if (_isLiquidityPair[_from]) {
+            // Buy tax 3% total
+            return ((_amount * buyTax) / 100, liqFee);
+        } else if (_isLiquidityPair[_to]) {
+            // Sell tax 5% total
+            liqFee = (_amount * liquidityFee) / 100;
+            return (((_amount * sellTax) / 100), liqFee);
+        } else {
+            return (0, liqFee);
         }
     }
 
@@ -493,75 +490,32 @@ contract BioKript is ERC20, Ownable, ReentrancyGuard  {
         uint256 amount
     ) internal override {
         require(from != address(0) && to != address(0), "ERC20: zero address");
-        require(balanceOf(to) + amount <= holdThreshold, "Err: Max hold");
+        require(
+            balanceOf(to) + amount <= holdThreshold ||
+                _isExcludedFromFees[from],
+            "Err: Max hold"
+        );
         uint256 tax = 0;
-        if (!_isExcludedFromFees[from] || !_isExcludedFromFees[to] && from == pancakeV2Pair || to == pancakeV2Pair) {
-            tax = calcTax(from, to, amount);
+        uint256 liqFee = 0;
+        if (
+            !_isExcludedFromFees[from] ||
+            _isLiquidityPair[from] ||
+            _isLiquidityPair[to]
+        ) {
+            (tax, liqFee) = calcTax(from, to, amount);
         }
-        super._transfer(from, to, amount - tax);
-        super._transfer(from, address(this), tax);
-        if(previousBalance[msg.sender]==0 && balanceOf(msg.sender)!=0 || previousBalance[to]==0 && balanceOf(to)!=0){
+
+        super._transfer(from, to, amount - (tax + liqFee));
+        if (tax > 0) super._transfer(from, feeReceiver, tax);
+        if (liqFee > 0) super._transfer(from, liquidityReceiver, liqFee);
+
         // Initiate with current balance (updated when claimed)
-            previousBalance[msg.sender]=balanceOf(msg.sender);
+        if ((previousBalance[from] == 0 && balanceOf(msg.sender) != 0)) {
+            previousBalance[from] = balanceOf(from);
         }
-    }
-
-    function swapAndLiquify(uint256 tokens) private {
-        // split the contract balance into halves
-        uint256 half = tokens.div(2);
-        uint256 otherHalf = tokens.sub(half);
-
-        // capture the contract's current BNB balance.
-        // this is so that we can capture exactly the amount of BNB that the
-        // swap creates, and not make the liquidity event include any BNB that
-        // has been manually sent to the contract
-        uint256 initialBalance = address(this).balance;
-
-        // swap tokens for BNB
-        swapTokensForBnb(half); // <- this breaks the Bnb -> HATE swap when swap+liquify is triggered
-
-        // how much Bnb did we just swap into?
-        uint256 newBalance = address(this).balance.sub(initialBalance);
-
-        // add liquidity to pancake
-        addLiquidity(otherHalf, newBalance);
-        emit SwapAndLiquify(half, newBalance, otherHalf);
-    }
-
-    function swapTokensForBnb(uint256 tokenAmount) public {
-        if (isSwapLimitEnabled) {
-            require(tokenAmount < MAX_SWAP_LIMIT, "Max swap reached");
+        if (previousBalance[to] == 0 && balanceOf(to) != 0) {
+            previousBalance[to] = balanceOf(to);
         }
-        // generate the pancake pair path of token -> weth
-        address[] memory path = new address[](2);
-        path[0] = address(this);
-        path[1] = pancakeV2Router.WETH();
-
-        _approve(address(this), address(pancakeV2Router), tokenAmount);
-
-        // make the swap
-        pancakeV2Router.swapExactTokensForBNBSupportingFeeOnTransferTokens(
-            tokenAmount,
-            0, // accept any amount of BNB
-            path,
-            address(this),
-            block.timestamp
-        );
-    }
-
-    function addLiquidity(uint256 tokenAmount, uint256 bnbAmount) private {
-        // approve token transfer to cover all possible scenarios
-        _approve(address(this), address(pancakeV2Router), tokenAmount);
-
-        // add the liquidity
-        pancakeV2Router.addLiquidityBNB{value: bnbAmount}(
-            address(this),
-            tokenAmount,
-            0, // slippage is unavoidable
-            0, // slippage is unavoidable
-            address(0),
-            block.timestamp
-        );
     }
 
     function setDistributor(address _address) external {
@@ -571,38 +525,71 @@ contract BioKript is ERC20, Ownable, ReentrancyGuard  {
 
     function distributeInvestors() external {
         require(msg.sender == distributor, "Err: only distributor");
-        require(block.timestamp>=nextDistr,"Distribution duration not passed!"); 
+        require(
+            block.timestamp >= nextDistr,
+            "Distribution duration not passed!"
+        );
         uint256 revenue = balanceOf(address(this));
         require(revenue > 0, "No revenue to distribute");
-        uint256 comTax = (revenue * 40) / 100;
-        _transfer(address(this), companyWallet, comTax);
+        uint256 companyTax = (revenue * 40) / 100;
+        _transfer(address(this), companyWallet, companyTax);
         uint256 burnTax = (revenue * 10) / 100;
         _transfer(address(this), burnWallet, burnTax);
-        uint256 safety = (revenue * 5) / 100;
-        _transfer(address(this), safetyFunds, safety);
-        uint256 reser = (revenue * 5) / 100;
-        _transfer(address(this), reserves, reser);
+        uint256 safetyTax = (revenue * 5) / 100;
+        _transfer(address(this), safetyFunds, safetyTax);
+        uint256 reserveTax = (revenue * 5) / 100;
+        _transfer(address(this), reserves, reserveTax);
         uint256 amount = (revenue * 40) / 100;
-        uint256 tSupply = totalSupply();
-        distributeTokens = ((amount * 10 ** 18) / tSupply) % 10 ** 18;  //allocating the number of tokens to be distributed
-        nextDistr = block.timestamp+2 minutes;
+        uint256 tSupply = totalSupply() / 10**18;
+        distributeTokens = (amount) / tSupply; //allocating the number of tokens to be distributed
+        nextDistr = block.timestamp + 30 days;
     }
 
     function claimRewards() external nonReentrant {
-        require(block.timestamp>=claimDur[msg.sender],"Wait for the next distribution");
-        uint256 distAmount = (distributeTokens * previousBalance[msg.sender]) / (10 ** 18);
+        uint256 distAmount = claimableRewardAmount(msg.sender);
+
+        require(
+            block.timestamp >= claimDur[msg.sender],
+            "Wait for the next distribution"
+        );
+        require(distAmount > 0, "No claimable rewards left");
+
         _transfer(address(this), msg.sender, distAmount);
-        previousBalance[msg.sender]=balanceOf(msg.sender);
+        previousBalance[msg.sender] = balanceOf(msg.sender);
         claimDur[msg.sender] = nextDistr;
     }
 
-    function TokenAddress() public view returns (address) {
-        return address(this);
+    function claimableRewardAmount(address wallet)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 distAmount = 0;
+        if (block.timestamp >= claimDur[wallet]) {
+            uint256 potential = previousBalance[wallet] / 10**18;
+            distAmount = (distributeTokens * (potential - 1));
+        }
+        return distAmount;
     }
 
-    function withdraw() external onlyController {   //empty the contract by transferring all tokens and bnb
+    function withdraw() external onlyController {
+        //empty the contract by transferring all tokens and bnb
         _transfer(address(this), controller, balanceOf(address(this)));
         (bool success, ) = controller.call{value: address(this).balance}("");
         require(success);
+    }
+
+    // Add liquidity pair
+    function addLiquidityPair(address _address, bool status)
+        external
+        onlyController
+    {
+        _isLiquidityPair[_address] = status;
+        emit UpdatePair(_address, status);
+    }
+
+    // Check if liquidity pair
+    function IsLiquidityPair(address _address) external view returns (bool) {
+        return _isLiquidityPair[_address];
     }
 }
